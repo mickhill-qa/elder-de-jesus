cdefe6d7d2db91b4dbc67371c79c0bc2807dd644
Mick Hill - Casa - Windows 8.1 <mickhill@hotmail.com.br>
2015-06-02 17:41:33 -0300

    conclusão

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,9 +2,14 @@
 
 class IndexController extends Controller
 {
+    public $banners;
+    protected $pasta_uploads;
+    
+    
     public function Index()
     {
-        $this->pagina_html();
+        $this->pasta_uploads = $this->site_model('ArquivosMickHill',$this->site_url,$this->site_caminhos['PUBLIC']);
+        $this->banners       = $this->pasta_uploads->listar_pasta('banners');
         $this->pagina_view();
     }
 }
